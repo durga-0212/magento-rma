cff69f9296410ea87c3f96b2a429ae0b6b2bc3f6
Durga <durga.sakhare@adapty.com>
2018-01-18 11:59:13 +0530

    navigation.php block

--- app/code/local/Test/Rma/Block/Account/Navigation.php
+++ app/code/local/Test/Rma/Block/Account/Navigation.php
@@ -1,9 +1,14 @@
 <?php
-
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-class Test_Rma_Block_Account_Navigation extends 
+class Test_Rma_Block_Account_Navigation extends Mage_Customer_Block_Account_Navigation
+{
+ 
+     public function removeLinkByName($name) {
+        unset($this->_links[$name]);
+    }
+}
 
