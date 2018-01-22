0016c027f34e54b881e3d4dc082011f964d6cd72
JJPL174 <Anjalee>
2018-01-22 15:18:22 +0530

    grid display

--- app/code/local/Test/Rma/Block/Adminhtml/Rma.php
+++ app/code/local/Test/Rma/Block/Adminhtml/Rma.php
@@ -1,5 +1,4 @@
 <?php
-
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,7 +7,8 @@
 class Test_Rma_Block_Adminhtml_Rma extends Mage_Adminhtml_Block_Widget_Grid_Container
 {
   public function __construct()
-  {
+  { 
+    
     $this->_controller = 'adminhtml_rma';
     $this->_blockGroup = 'rma';
     $this->_headerText = Mage::helper('rma')->__('RMA Grid');
