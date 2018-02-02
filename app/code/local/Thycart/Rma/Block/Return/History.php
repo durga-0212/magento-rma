84eef72bd196ef3dc131952e0ae5cf0e16f1fec7
Durga <durga.sakhare@adapty.com>
2018-02-02 18:23:12 +0530

    return history changes

--- app/code/local/Thycart/Rma/Block/Return/History.php
+++ app/code/local/Thycart/Rma/Block/Return/History.php
@@ -10,14 +10,32 @@ class Thycart_Rma_Block_Return_History extends Mage_Core_Block_Template
         parent::__construct(); 
         $this->setTemplate('rma/return/history.phtml'); 
         Mage::app()->getFrontController()->getAction()->getLayout()->getBlock('root')->setHeaderTitle(Mage::helper('rma')->__('My Returns'));
-        $returns=Mage::getResourceModel('rma/order_collection')
-                    ->join(array('sfoi' => 'sales/order_item'), 'main_table.order_id = sfoi.order_id', array(
-                    'product_id'))
-                 ->join(array('sfo' => 'sales/order'), 'main_table.increment_id = sfo.increment_id', array(
-                    'grand_total'))                    
-                    ->addFieldToFilter('customer_id', Mage::getSingleton('customer/session')->getCustomer()->getId())                
-                ->setOrder('date_requested','desc');        
+        $returns=Mage::getModel('rma/order')->getAllRmas();
+//                    ->join(array('sfoi' => 'sales/order_item'), 'main_table.order_id = sfoi.order_id', array(
+//                    'product_id'))
+//                 ->join(array('sfo' => 'sales/order'), 'main_table.increment_id = sfo.increment_id', array(
+//                    'grand_total'))                    
+//                    ->addFieldToFilter('customer_id', Mage::getSingleton('customer/session')->getCustomer()->getId())                
+//                ->setOrder('date_requested','desc');        
         $this->setReturns($returns);               
+    }
+    
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+
+        $pager = $this->getLayout()->createBlock('page/html_pager')
+                ->setCurPage(0)
+                ->setLimit(1)
+            ->setCollection($this->getReturns());
+        $this->setChild('pager', $pager);
+        $this->getReturns()->load();
+        return $this;
+    }
+
+    public function getPagerHtml()
+    {
+        return $this->getChildHtml('pager');
     }
     
     public function getViewUrl($order)
