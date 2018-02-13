030374faf47b7951bcfba90a7f3e9b350de8a7de
JJPL174 <Anjalee>
2018-02-13 19:32:48 +0530

    methods in model

--- app/code/local/Thycart/Rma/Model/Rma/Attributes.php
+++ app/code/local/Thycart/Rma/Model/Rma/Attributes.php
@@ -11,5 +11,14 @@ class Thycart_Rma_Model_Rma_Attributes extends Mage_Core_Model_Abstract
     {
         $this->_init('rma/rma_attributes');
     }
+    
+    public function getAttributesCollection($id)
+    {
+        $products=Mage::getResourceModel('rma/rma_attributes_collection')
+                ->join(array('rfoi' => 'rma/rma_item'), 'main_table.rma_entity_id = rfoi.rma_entity_id', array(
+                    '*'))                
+                ->addFieldToFilter('main_table.rma_entity_id',$id);              
+        return $products;
+    }
 }
 
