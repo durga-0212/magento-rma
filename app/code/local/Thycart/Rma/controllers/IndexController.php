ba58952fd05a90baefd161ca283c6f6f1fc94f49
Rajendra N <rajendra.namdeo@adapty.com>
2018-02-14 18:54:02 +0530

    save function changes for cancel order

--- app/code/local/Thycart/Rma/controllers/IndexController.php
+++ app/code/local/Thycart/Rma/controllers/IndexController.php
@@ -102,37 +102,44 @@ class Thycart_Rma_IndexController extends Mage_Core_Controller_Front_Action
     
     public function saveAction()
     {
-        $data = $this->getRequest()->getParams();       
+        $data = $this->getRequest()->getParams();
+        $status = 'pending';
+        if(isset($data['cancelType']) && $data['cancelType'] ==1)
+        {
+            $status = 'canceled';
+        }
         $orderModel = Mage::getModel('rma/order'); 
         $date = Mage::getModel('core/date')->date('Y-m-d H:i:s');
         $customerModel = Mage::getSingleton('customer/session')->getCustomer();
-        $orderModel->setData(array('order_id'=>$data['order_id'],'increment_id'=>$data['increment_id'],'order_increment_id'=>$data['increment_id'],'order_date'=>$data['order_date'],'date_requested'=>$date,'store_id'=> $data['store_id'],'customer_id'=>$customerModel->getEntityId(),'customer_name'=>$customerModel->getName(),'customer_email'=>$customerModel->getEmail(),'status'=>'pending'));
+        $orderModel->setData(array('order_id'=>$data['order_id'],'increment_id'=>$data['increment_id'],'order_increment_id'=>$data['increment_id'],'order_date'=>$data['order_date'],'date_requested'=>$date,'store_id'=> $data['store_id'],'customer_id'=>$customerModel->getEntityId(),'customer_name'=>$customerModel->getName(),'customer_email'=>$customerModel->getEmail(),'status'=>$status));
         if($orderModel->save())
         {
-         foreach ($data['Product'] as $key => $value) 
-        {
-            if($value['checked'])
+            foreach ($data['Product'] as $key => $value) 
             {
-                $item_data=array(
-                    'rma_entity_id' => $orderModel->getId(),
-                    'qty_ordered'  => $value['qty_ordered'],
-                    'product_name' => $value['name'],
-                    'product_sku' => $value['sku'],
-                    'order_item_id' => $value['item_id'],
-                    'qty_requested' => $value['qty_requested'],
-                    'product_options' => $value['product_options'],
-                    'item_status' => 'pending'
-                );
-              $rmaItemModel = Mage::getModel('rma/rma_item');  
-              $rmaItemModel->setData($item_data);
-              $rmaItemModel->save();
-              $this->_redirect('*/*/index');
-            }           
-        }  
-          
-            $rmaHistoryModel = Mage::getModel('rma/rma_history');
-            $rmaHistoryModel->setData(array('rma_entity_id'=> $orderModel->getId(),'is_visible_on_front'=>1,'comment'=>'Your RMA request has been placed','status'=>'pending','created_at'=>$date,'is_admin'=>1));
-            $rmaHistoryModel->save();
+                if($value['checked'] ||  $data['cancelType'])
+                {
+                    $item_data=array(
+                        'rma_entity_id' => $orderModel->getId(),
+                        'qty_ordered'  => $value['qty_ordered'],
+                        'product_name' => $value['name'],
+                        'product_sku' => $value['sku'],
+                        'order_item_id' => $value['item_id'],
+                        'qty_requested' => $value['qty_requested'],
+                        'product_options' => $value['product_options'],
+                        'item_status' => $status
+                    );
+                  $rmaItemModel = Mage::getModel('rma/rma_item');  
+                  $rmaItemModel->setData($item_data);
+                  $rmaItemModel->save();
+                  $this->_redirect('*/*/index');
+                }           
+            }  
+            if(!isset($data['cancelType']) || $data['cancelType'] ==0)
+            {
+                $rmaHistoryModel = Mage::getModel('rma/rma_history');
+                $rmaHistoryModel->setData(array('rma_entity_id'=> $orderModel->getId(),'is_visible_on_front'=>1,'comment'=>'Your RMA request has been placed','status'=>'pending','created_at'=>$date,'is_admin'=>1));
+                $rmaHistoryModel->save();
+            }
             $rmaAttributeModel = Mage::getModel('rma/rma_attributes');
             $rmaAttributeModel->setData(array('rma_entity_id'=> $orderModel->getId(),'resolution'=>$data['resolution_type'],'delivery_status'=>$data['delivery_status'],'reason'=>$data['reason'],'created_at'=>$date));
             $rmaAttributeModel->save();      
