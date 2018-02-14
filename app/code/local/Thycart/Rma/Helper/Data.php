53fd3d46512b43db7200ccc9a32fd974dc04d164
Durga <durga.sakhare@adapty.com>
2018-02-14 21:28:14 +0530

    data.php

--- app/code/local/Thycart/Rma/Helper/Data.php
+++ app/code/local/Thycart/Rma/Helper/Data.php
@@ -19,5 +19,41 @@ class Thycart_Rma_Helper_Data extends Mage_Core_Helper_Abstract
         $invoiceIds = $orderObject->getInvoiceCollection()->getAllIds();
         return $invoiceIds;
     }
+    
+     public function getTrackingNumber()
+    {
+        $digits_needed = 8;
+        $random_number = ''; // set up a blank string
+        $count = 0;  
+        $carriers=$this->getEnabledshippingmethods(); 
+        
+         $random_number .= $carriers;        
+          while ($count < $digits_needed) {
+            $random_digit = mt_rand(0, 9);                     
+            $random_number .= $random_digit;
+            $count++;
+           }          
+        return $random_number;
+    }
+    
+    public function getEnabledshippingmethods()
+    {
+        $methods = Mage::getSingleton('shipping/config')->getAllCarriers();       
+        $shipMethodCollection = new Varien_Data_Collection();
+        foreach ($methods as $code => $carrier) {            
+                $carriers[$code] = $carrier->getConfigData('title');           
+        }        
+        $arr=array();
+        foreach($carriers as $key=> $value)
+        {
+            $arr[$key]=$key;           
+        }
+         $k = array_rand($arr);        
+         $v = $arr[$k];        
+        return $v;
+    }
+    
+    
+    
 }
 ?>
