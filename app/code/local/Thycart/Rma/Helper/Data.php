2632de3025e59831f6c69fdf0757108a3d31bd33
Ritesh Shukla <ritesh.shukla@adapty.com>
2018-02-24 19:02:05 +0530

    functions for get email body and send email

--- app/code/local/Thycart/Rma/Helper/Data.php
+++ app/code/local/Thycart/Rma/Helper/Data.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+require_once('phpmailer/class.phpmailer.php');
 class Thycart_Rma_Helper_Data extends Mage_Core_Helper_Abstract
 {     
     public function getAttributeOptionValues($attribute_code) 
@@ -72,34 +73,151 @@ class Thycart_Rma_Helper_Data extends Mage_Core_Helper_Abstract
         return $ordershipdata;    
     }
     
-    public function sendMail($from,$to,$subject,$body,$link='')
+    public function sendMail($to, $recepientName, $subject, $orderid, $productName, $message, $link='')
     {
-        require_once('phpmailer/class.phpmailer.php');
-        $mail = new PHPMailer(); // create a new object
-        $mail->IsSMTP(); // enable SMTP
-        $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-        $mail->SMTPAuth = true; // authentication enabled
-        $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-        $mail->Host = "smtp.gmail.com";
-        $mail->Port = 465; // or 587
-        $mail->IsHTML(true);
-        $mail->Username = $from;
-        $mail->Password = "";
-        $mail->SetFrom($from);
-        $mail->Subject = $subject;
-        $mail->Body = $body.'<br>'.$link;
-        $mail->AddAddress($to);
-
-        if(!$mail->Send()) 
+        if(empty($to) || empty($recepientName) || empty($subject) ||empty($orderid) || empty($productName) || empty($message))
         {
-           return false;
-        } 
-        else 
-        {
-           return true;
+            return false;
         }
+        if(PHP_MAILER)
+        { 
+            try
+            {
+                $mail = new PHPMailer();
+                $mail->IsSMTP();
+                $mail->SMTPDebug = 1; 
+                $mail->SMTPAuth = true;
+                $mail->SMTPSecure = 'ssl';
+                $mail->Host = "smtp.gmail.com";
+                $mail->Port = 465;
+                $mail->IsHTML(true);
+                $mail->Username = Mage::getStoreConfig('rma_email/email_group/sender_email');
+                $mail->Password = 'shukla0912';
+                $mail->SetFrom(Mage::getStoreConfig('rma_email/email_group/sender_email'));
+                $mail->Subject = $subject;
+                $body = $message;
+                $mail->Body = Mage::helper('rma')->getEmailBody($orderid, $productName, $message ,$link);
+                $mail->AddAddress($to);
+            }
+            catch (Exception $e)
+            {
+                echo $e->getMessage();
+                return false;
+            }
+            
+            if(!$mail->Send()) 
+            {
+               return false;
+            }   
+        }
+        if(MAGENTO_MODEL)
+        {
+            $mail = Mage::getModel('core/email');
+            $mail->setToName($recepientName);
+            $mail->setToEmail($to);
+            $mail->setBody(Mage::helper('rma')->getEmailBody($orderid, $productName, $message ,$link));
+            $mail->setSubject($subject);
+            $mail->setFromEmail(Mage::getStoreConfig('rma_email/email_group/sender_email'));
+            $mail->setType('html');
+            try
+            {
+                if(!$mail->send())
+                {
+                    return false;
+                }
+            }
+            catch (Exception $e)
+            {
+                Mage::getSingleton('core/session')->addError('Unable to send.');
+                return false;
+            }   
+        }
+        if(ZEND_FUNCTION)
+        {
+            $sender_email = 'ritesh.shukla@adapty.com';
+            $sender_name = "sender name";
+
+            $mail = new Zend_Mail();
+            $mail->setBodyHtml(Mage::helper('rma')->getEmailBody($orderid, $productName, $message ,$link)); 
+            $mail->setFrom(Mage::getStoreConfig('rma_email/email_group/sender_email'),Mage::getStoreConfig('rma_email/email_group/sender_name'));
+            $mail->addTo($to, 'customer');
+            //$mail->addCc($cc, $ccname);    //can set cc
+            //$mail->addBCc($bcc, $bccname);    //can set bcc
+            $mail->setSubject($subject);
+            try
+            {
+                if(!$mail->send())
+                {
+                    return false;
+                }
+            }
+            catch(Exception $ex)
+            {
+                die("Error sending mail to $to,$error_msg");
+            }
+        }
+        if(Mage::getStoreConfig('rma_email/email_group/transactional_email'))
+        {
+            $templateId     = 1;     
+            $senderName     = Mage::getStoreConfig('rma_email/email_group/sender_name');
+            $senderEmail    = Mage::getStoreConfig('rma_email/email_group/sender_email');        
+            $sender         = array('name' => $senderName,
+                                    'email' => $senderEmail
+                                   );              
+            $storeId = Mage::app()->getStore()->getId();
+            $vars    = array('message' => $message,
+                             'orderid'=> $orderid,
+                             'productDetails'=>$productName
+                            );
+            
+            if(!empty($link))
+            {
+                $vars['link'] = $link;
+            }
+            try
+            {
+                $translate  = Mage::getSingleton('core/translate');
+                Mage::getModel('core/email_template')
+                        ->sendTransactional($templateId, $sender, $to, $recepientName, $vars, $storeId);    
+                $translate->setTranslateInline(true);   
+            }
+            catch (Exception $e)
+            {
+                echo $e->getMessage();
+                return false;
+            }
+        }     
+        return true;
     }
-    
+
+    public function getEmailBody($orderid, $productName, $message , $link='')
+    { 
+        $body ='';
+        $emailTemplateVars = array();
+        if(empty($orderid) || empty($productName))
+        {
+            return false;
+        }
+        if(!empty($link))
+        {
+            $emailTemplateVars['link'] = $link;
+        }
+        $emailTemplateVars['product_details'] = $productName;
+        $emailTemplateVars['orderid'] = $orderid;
+        $emailTemplateVars['message'] = $message;
+        try
+        {
+            $emailTemplate  = Mage::getModel('core/email_template')
+                            ->loadDefault('rma_template');
+            $body = $emailTemplate->getProcessedTemplate($emailTemplateVars);
+        }
+        catch(Exception $e)
+        {
+           echo $e->getMessage(); 
+           return;
+        }
+       return $body;
+    }
     public function updateInventory($productId,$qtyApproved)
     {
         $inventoryModel = Mage::getModel('cataloginventory/stock_item')->load($productId);
@@ -115,4 +233,3 @@ class Thycart_Rma_Helper_Data extends Mage_Core_Helper_Abstract
     }
     
 }
-?>
