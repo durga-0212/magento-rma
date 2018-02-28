2701622053b177567d29b36356f8bceced030e33
Ritesh Shukla <ritesh.shukla@adapty.com>
2018-02-28 17:45:40 +0530

    removed empty value from dropdown and removed payment reqquest when its return recieved

--- app/code/local/Thycart/Rma/Block/Adminhtml/Rma/Edit/Tab/Items/Grid/Column/Renderer/Textselect.php
+++ app/code/local/Thycart/Rma/Block/Adminhtml/Rma/Edit/Tab/Items/Grid/Column/Renderer/Textselect.php
@@ -9,22 +9,27 @@ class Thycart_Rma_Block_Adminhtml_Rma_Edit_Tab_Items_Grid_Column_Renderer_Textse
         $selectName = 'items[' . $row->getId() . '][' . $this->getColumn()->getId() . ']';
         $html = '<select name="'. $selectName .'" class="action-select required-entry"'.$disabled.'>';
         $value = $row->getData($this->getColumn()->getIndex());
-        $html.= '<option value=""></option>';
         $statusArray = $this->getColumn()->getOptions();
         $priorityArray = array_values($statusArray);
         $priorityArray =array_reverse($priorityArray);
+
         for ($i=0; $i < count($priorityArray); $i++)
         { 
             $status = strtolower($priorityArray[$i]);
+            $selected = ($status == $value && (!is_null($value)) ? ' selected="selected"' : '' );
+            if($value == 'return received')
+            {
+                $html.= '<option  value="' . $value . '"' . $selected .'>' . $value . '</option>';
+                break;
+            }
             if($status == $value)
             {
-                $selected = ($status == $value && (!is_null($value)) ? ' selected="selected"' : '' );
                 if($value != 'canceled')
                 {
                     $html.= '<option  value="' . strtolower($priorityArray[$i+1]) . '"' . $selected .'>' . $priorityArray[$i+1] . '</option>';
                     if($value == 'processing')
                     {
-                        $html.= '<option  value="' . strtolower(end($priorityArray)) . '"' . $selected .'>' . strtolower(end($priorityArray)) . '</option>';
+                        $html.= '<option  value="' . strtolower(end($priorityArray)) . '"' . $selected .'>' . end($priorityArray) . '</option>';
                     }
                 }
                 $html.= '<option  value="' . $status . '"' . $selected .'>' . $status . '</option>';
