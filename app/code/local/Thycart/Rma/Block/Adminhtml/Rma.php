1d2105671afe2149a3e389f5b6fa9712a7dce7a8
Durga <durga.sakhare@adapty.com>
2018-03-07 23:18:13 +0530

    rma change grid

--- app/code/local/Thycart/Rma/Block/Adminhtml/Rma.php
+++ app/code/local/Thycart/Rma/Block/Adminhtml/Rma.php
@@ -6,14 +6,13 @@
  */
 class Thycart_Rma_Block_Adminhtml_Rma extends Mage_Adminhtml_Block_Widget_Grid_Container
 {
-  public function __construct()
-  {     
-    $this->_controller = 'adminhtml_rma';
-    $this->_blockGroup = 'rma';
-    $this->_headerText = Mage::helper('rma')->__('RMA Grid');
-   // $this->_addButtonLabel = Mage::helper('rma')->__('Add Item');
-    parent::__construct();
-    $this->_removeButton('add');
-  }
+    public function __construct()
+    {     
+        $this->_controller = 'adminhtml_rma';
+        $this->_blockGroup = 'rma';
+        $this->_headerText = Mage::helper('rma')->__('RMA Grid');
+        parent::__construct();
+        $this->_removeButton('add');
+    }
 }
 
